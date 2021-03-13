b90b51db37e0556aca7fc3e0904fbd8c262d534c
tanakaderoy <42681052+tanakaderoy@users.noreply.github.com>
2021-03-13 11:56:07 -0500

    add react-native-windows support

---
+++ windows/tanakaanime/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::tanakaanime::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::tanakaanime::implementation
+
+
