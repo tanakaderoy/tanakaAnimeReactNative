0d2f202114fd2ad5f79d88708c348acc0e22c70d
tanakaderoy <42681052+tanakaderoy@users.noreply.github.com>
2021-03-13 13:25:52 -0500

    add tabbar icons

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React, {ReactNode} from 'react';
 import {StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {HomeStack} from './src/HomeStack';
@@ -25,13 +27,38 @@ type TabParamList = {
 };
 
 const App: () => ReactNode = () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStack} />
-      <Tab.Screen name="Search" component={SearchStack} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  </NavigationContainer>
+  <SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = '';
+            //Ionicons
+
+            switch (route.name) {
+              case 'Home':
+                iconName = focused ? 'home' : 'home-outline';
+                break;
+              case 'Search':
+                iconName = focused ? 'search-circle' : 'search-circle-outline';
+                break;
+              case 'Settings':
+                iconName = focused ? 'settings' : 'settings-outline';
+                break;
+              default:
+                break;
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{activeTintColor: 'tomato', inactiveTintColor: 'gray'}}>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Search" component={SearchStack} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  </SafeAreaProvider>
 );
 
 export default App;
